89cb80f0a2fe3780acc9545095647f6490938cb8
taniakornilova <t.kornilova04@gmail.com>
2018-07-09 19:39:02 +0300

    task 4, 4 type of loop

--- task4.py
+++ task4.py
@@ -17,8 +17,16 @@ len_args = len(arguments)
 #for i in range(1, len_args+1):
 #	read_args(sys.argv[i])
 
-i = 1
-while i <= len_args:
-	read_args(sys.argv[i])
-	i += 1
+#i = 1
+#while i <= len_args:
+#	read_args(sys.argv[i])
+#	i += 1
 
+i = 1
+while 1:
+	try:
+		read_args(sys.argv[i])
+		i += 1
+	except IndexError:
+		break
+		

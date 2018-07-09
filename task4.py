f8a939c4eb81f0d12b1b33b9db88457fadc0592e
taniakornilova <t.kornilova04@gmail.com>
2018-07-09 17:36:22 +0300

    task 4 2 type of loop

--- task4.py
+++ task4.py
@@ -8,5 +8,9 @@ def read_args(argument):
 	else:
 		print("ln(%g) = %g" % (num, m.log(num)))
 
-for r in sys.argv[1:]:
-	read_args(r)
+#for r in sys.argv[1:]:
+#	read_args(r)
+arguments = sys.argv[1:]
+len_args = len(arguments)
+for i in range(1, len_args+1):
+	read_args(sys.argv[i])

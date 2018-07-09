db68ea11e270cf31d3f2c683c2033e05fce487bf
taniakornilova <t.kornilova04@gmail.com>
2018-07-10 02:36:43 +0300

    task 7

---
+++ task7.py
@@ -0,0 +1,23 @@
+#analytical solution 0.30555556
+import random
+
+
+def MMC(n):
+	count_six = 0
+	for i in range(n):
+		a = random.randint(1, 6)
+		b = random.randint(1, 6)
+		if (a == 6 or b == 6):
+			count_six += 1
+	return count_six/n
+	
+n = int(input())
+analyt_sol = 0.305555
+sol = MMC(n)
+while abs(sol - analyt_sol) >= 0.0001:
+	n = n*2
+	sol = MMC(n)
+
+print(n)
+print(sol)
+	

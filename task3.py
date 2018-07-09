3fe049c1f7cbd109b0e47fb8035500a6ed44d845
taniakornilova <t.kornilova04@gmail.com>
2018-07-09 15:45:10 +0300

    fixed index

--- task3.py
+++ task3.py
@@ -1,11 +1,14 @@
 import sys , random
 
+
 def  compute(n):
-	i = 0; s = 0
-	while i  <= n:
-		s +=  random.random ()
-		i += 1
-	return s/n
+  i = 0; s = 0
+  while i  < n:
+    s +=  random.random ()
+    i += 1
+  return s/n
+
+
 n = int(sys.argv[1])
 print  ("average of %d random  numbers  is %g" %(n, compute(n)))
 
